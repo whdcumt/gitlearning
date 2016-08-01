06233933cea5cec87e2bdc01b3dd0efdf380c989
whdcumt <wanghongdong0@126.com>
2016-08-01 12:58:25 +0800

    冲突解决后回头来继续之前的项目

--- git_learning/git_learning.cpp
+++ git_learning/git_learning.cpp
@@ -2,11 +2,15 @@
 //
 
 #include "stdafx.h"
-
-
+#include<iostream>
+using namespace std;
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int a,b;
+	int a,b,c;
+    cout << "请输入3个数字，按回车结束" << endl;
+    cin >>a>>b>>c;
+    cout << a <<b<<c<< endl;
+	cout << "修复bug结束" << endl;
 	return 0;
 }
 
